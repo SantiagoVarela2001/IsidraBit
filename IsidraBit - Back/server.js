c24bdd821d15b254679b4fd5056eabada462118e
Santiago Varela <santiago.varela@hotmail.com>
2024-07-16 19:58:40 -0300

    arreglo cors nuevo servidor

--- IsidraBit - Back/server.js
+++ IsidraBit - Back/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import RouterServer from './router/router.js';
 import db from './model/DAOs/db.js';
 import cors from 'cors';
-import bodyParser from 'body-parser'; // Importar body-parser
+import bodyParser from 'body-parser';
 import config from './config.js';
 
 class Server {
@@ -18,8 +18,12 @@ class Server {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
 
+        // Configuración de CORS
         this.app.use(cors({
-            origin: "https://isidrabit.netlify.app"
+            origin: "https://isidrabit.netlify.app",
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+            preflightContinue: false,
+            optionsSuccessStatus: 204
         }));
 
         /* ------------------------------------------------------------- */
@@ -30,18 +34,30 @@ class Server {
         /* ------------------------------------------------------------- */
         /*                      Servidor LISTEN                          */
         /* ------------------------------------------------------------- */
-        if (config.MODO_PERSISTENCIA == 'MONGODB') {
-            await db.conectar();
+        if (config.MODO_PERSISTENCIA === 'MONGODB') {
+            try {
+                await db.conectar();
+                console.log('Conexión a MongoDB establecida');
+            } catch (error) {
+                console.error('Error conectando a la base de datos:', error);
+                process.exit(1); // Salir si no se puede conectar a la base de datos
+            }
         }
+
         const PORT = this.port;
-        this.server = this.app.listen(PORT, () => console.log(`Servidor express escuchando en el puerto https://isidrabit.onrender.com`));
-        this.server.on('error', error => console.log('Servidor express en error:', error));
+        this.server = this.app.listen(PORT, () => 
+            console.log(`Servidor express escuchando en el puerto ${PORT}`)
+        );
+
+        this.server.on('error', error => 
+            console.error('Servidor express en error:', error)
+        );
 
         return this.app;
     }
 
     async stop() {
-        this.server.close();
+        this.server.close(() => console.log('Servidor detenido'));
         await db.desconectar();
     }
 }
